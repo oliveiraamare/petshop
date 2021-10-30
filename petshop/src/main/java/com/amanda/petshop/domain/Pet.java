4c0a9f7ebdaf1f1d0225aa86e12d41652a0b14ae
oliveiraamare <amare.os@hotmail.com>
2021-10-30 00:10:56 -0300

    Ultimos relacionamentos: Pet, Servico e Produto e Finalizado instancias

--- petshop/src/main/java/com/amanda/petshop/domain/Pet.java
+++ petshop/src/main/java/com/amanda/petshop/domain/Pet.java
@@ -1,6 +1,8 @@
 package com.amanda.petshop.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Pet implements Serializable{
@@ -28,6 +31,9 @@ public class Pet implements Serializable{
 	@JoinColumn(name = "id_raca")
 	private Raca raca;
 
+	@OneToMany(mappedBy = "pet")
+	private List<Servico> servicos = new ArrayList<>();
+	
 	public Pet() {
 
 	}
