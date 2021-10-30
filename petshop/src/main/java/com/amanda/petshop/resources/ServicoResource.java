0a556dc33d661f676f80508bca37c9a4cfe11493
oliveiraamare <amare.os@hotmail.com>
2021-10-30 00:28:32 -0300

    Disponibilizado Servicos/{id} e corrigido referencias ciclicas

---
+++ petshop/src/main/java/com/amanda/petshop/resources/ServicoResource.java
@@ -0,0 +1,25 @@
+package com.amanda.petshop.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amanda.petshop.domain.Servico;
+import com.amanda.petshop.service.ServicoService;
+
+@RestController
+@RequestMapping(value = "/servicos")
+public class ServicoResource {
+
+	@Autowired
+	ServicoService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {		
+		Servico obj = service.find(id);		
+		return ResponseEntity.ok().body(obj);
+	}
+}
