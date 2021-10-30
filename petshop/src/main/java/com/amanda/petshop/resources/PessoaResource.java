00e9d90bf511fce8be1dd8106add11a3c363d7e9
oliveiraamare <amare.os@hotmail.com>
2021-10-30 01:05:40 -0300

    Listando Pessoas com dto

--- petshop/src/main/java/com/amanda/petshop/resources/PessoaResource.java
+++ petshop/src/main/java/com/amanda/petshop/resources/PessoaResource.java
@@ -2,6 +2,7 @@ package com.amanda.petshop.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.amanda.petshop.domain.Pessoa;
+import com.amanda.petshop.dto.PessoaDTO;
 import com.amanda.petshop.service.PessoaService;
 
 @RestController
@@ -47,10 +49,12 @@ public class PessoaResource {
 		service.delete(id);		
 		return ResponseEntity.noContent().build();
 	}
-
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Pessoa>> findAll() {		
+	public ResponseEntity<List<PessoaDTO>> findAll() {		
+
 		List<Pessoa> list = service.findAll();		
-		return ResponseEntity.ok().body(list);
+		List<PessoaDTO> listDto = list.stream().map(obj -> new PessoaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
