cef899baf2c2f33ebb33c9eb7b9da750898eea5d
oliveiraamare <amare.os@hotmail.com>
2021-10-30 00:40:36 -0300

    CRUD de Categoria (Post, Put, Delete) + Manipulação do Exception

--- petshop/src/main/java/com/amanda/petshop/service/CategoriaService.java
+++ petshop/src/main/java/com/amanda/petshop/service/CategoriaService.java
@@ -3,10 +3,12 @@ package com.amanda.petshop.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.amanda.petshop.domain.Categoria;
 import com.amanda.petshop.repository.CategoriaRepository;
+import com.amanda.petshop.service.exceptions.DataIntegrityException;
 import com.amanda.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -19,5 +21,25 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		
 		return obj.orElseThrow( () -> new ObjetoNaoEncontradoException("Objeto não encontrado. ID: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}	
+
+	public void delete(Integer id) {
+		find(id);		
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Categoria possui produtos, não é possível deletar!");
+		}
+
 	}
 }
