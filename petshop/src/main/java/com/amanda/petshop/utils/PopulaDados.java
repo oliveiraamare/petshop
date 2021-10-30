12d17044548c1d7bd6e58e7c713b8211983d9523
oliveiraamare <amare.os@hotmail.com>
2021-10-29 22:05:39 -0300

    Relacionamento entre Estado e Cidades

--- petshop/src/main/java/com/amanda/petshop/utils/PopulaDados.java
+++ petshop/src/main/java/com/amanda/petshop/utils/PopulaDados.java
@@ -8,12 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.amanda.petshop.domain.Categoria;
+import com.amanda.petshop.domain.Cidade;
 import com.amanda.petshop.domain.Especie;
+import com.amanda.petshop.domain.Estado;
 import com.amanda.petshop.domain.Pet;
 import com.amanda.petshop.domain.Produto;
 import com.amanda.petshop.domain.Raca;
 import com.amanda.petshop.repository.CategoriaRepository;
+import com.amanda.petshop.repository.CidadeRepository;
 import com.amanda.petshop.repository.EspecieRepository;
+import com.amanda.petshop.repository.EstadoRepository;
 import com.amanda.petshop.repository.PetRepository;
 import com.amanda.petshop.repository.ProdutoRepository;
 import com.amanda.petshop.repository.RacaRepository;
@@ -35,6 +39,12 @@ public class PopulaDados {
 
 	@Autowired
 	PetRepository petRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
+
+	@Autowired
+	CidadeRepository cidadeRepository;
 	
 	@PostConstruct
 	public void cadastrar() {
@@ -78,5 +88,18 @@ public class PopulaDados {
 		racaRepository.saveAll(Arrays.asList(rac1, rac2, rac3));
 
 		petRepository.saveAll(Arrays.asList(pet1, pet2, pet3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Belo Horizonte", est1);
+		Cidade c2 = new Cidade(null, "Capelinha", est1);
+		Cidade c3 = new Cidade(null, "São Paulo", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1, c2));
+		est2.getCidades().addAll(Arrays.asList(c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 }
